82dbf757c84810b29bf36e954d3c8bda1d2d1cd7
ptQa <ptqa.mail@gmail.com>
2016-01-29 00:08:56 +0100

    set open files

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,11 @@
-default['fabio']['init_style'] = 'runit'
+default['fabio']['init_style']   = 'runit'
+default['fabio']['open_files']   = 65535
 default['fabio']['config']       = {}
 default['fabio']['install_path'] = '/usr/local/bin/fabio'
 
-default['fabio']['conf_dir'] = '/etc/fabio'
-default['fabio']['log_dir'] = '/var/log/fabio'
+default['fabio']['conf_dir']     = '/etc/fabio'
+default['fabio']['log_dir']      = '/var/log/fabio'
 
 # Installation source
-default['fabio']['release_url'] = 'https://github.com/eBay/fabio/releases/download/'
-default['fabio']['version'] = '1.0.8'
+default['fabio']['release_url']  = 'https://github.com/eBay/fabio/releases/download/'
+default['fabio']['version']      = '1.0.8'
--- recipes/configure.rb
+++ recipes/configure.rb
@@ -23,5 +23,7 @@ template "#{node['fabio']['conf_dir']}/fabio.properties" do
 end
 
 poise_service 'fabio' do
-  command "#{node['fabio']['install_path']} -cfg #{node['fabio']['conf_dir']}/fabio.properties"
+  # hacky way to set ulimit without modifying service template
+  command "bash -c 'ulimit -n #{node['fabio']['open_files']}; \
+    exec #{node['fabio']['install_path']} -cfg #{node['fabio']['conf_dir']}/fabio.properties"
 end
