fe85d8411f3f3492dc9c7f59a31640ccf495f481
Alex Bacchin <alexbacchin@hotmail.com>
2017-03-01 13:09:38 +1100

    copied from community chef-fabio

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name             'fabio'
-maintainer       'ptQa'
-maintainer_email 'ptqa.mail@gmail.com'
+name             'didata-fabio'
+maintainer       'Alex Bacchin'
+maintainer_email 'alex.bacchin@dimensiondata.com'
 license          'Apache 2.0'
 description      'Installs and configures fabio (zero-conf load balancing HTTP router for deploying microservices managed by consul)'
-version          '1.2.1'
+version          '1.2.2'
 
 supports 'ubuntu'
 supports 'debian'
